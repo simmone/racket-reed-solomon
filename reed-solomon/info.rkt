6bc61d637b2ff5fe914b6e6ac77121620002f304
Chen Xiao <chenxiao770117@gmail.com>
2018-12-03 10:46:18 +0800

    update

--- reed-solomon/info.rkt
+++ reed-solomon/info.rkt
@@ -2,7 +2,7 @@
 (define scribblings
   '(("scribble/reed-solomon.scrbl" (multi-page) (tool 100))))
 
-(define compile-omit-paths '("tests"))
-(define test-omit-paths '("tests"))
+(define compile-omit-paths '("tests ref"))
+(define test-omit-paths '("tests ref"))
 
 
