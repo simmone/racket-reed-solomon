158f60e448cc0c8b2b651b3980d0f1716f93a646
simmone <chenxiao770117@gmail.com>
2018-12-08 17:42:37 +0800

    update

--- reed-solomon/lib/decode/syndrome.rkt
+++ reed-solomon/lib/decode/syndrome.rkt
@@ -7,15 +7,15 @@
 (define (get-syndromes raw_list error_code_length)
   (let loop ([loop_count 0]
              [result_list '()])
-    (if (< loop_count 14)
+    (if (< loop_count error_code_length)
         (loop
          (add1 loop_count)
          (cons
           (let xor-loop ([loop_list raw_list]
                          [last_result loop_count])
             (if (not (null? loop_list))
-                (xor-loop (cdr loop_list) (bitwise-xor last_result (car loop_list)))
+                (xor-loop (cdr loop_list) (* (bitwise-xor last_result (car loop_list)))
                 last_result))
           result_list))
-        result_list)))
+        result_list))))
           
