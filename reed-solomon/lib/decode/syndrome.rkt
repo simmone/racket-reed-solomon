059792be181e245286adba01f7f43f3747588a84
simmone <chenxiao770117@gmail.com>
2018-12-09 12:10:16 +0800

    update

--- reed-solomon/lib/decode/syndrome.rkt
+++ reed-solomon/lib/decode/syndrome.rkt
@@ -1,21 +1,44 @@
 #lang racket
 
+(require "../share/gf.rkt")
+
 (provide (contract-out
           [get-syndromes (-> (listof exact-integer?) natural? (listof exact-integer?))]
           ))
 
 (define (get-syndromes raw_list error_code_length)
-  (let loop ([loop_count 0]
+  (let loop ([loop_a_count 3]
              [result_list '()])
-    (if (< loop_count error_code_length)
+    (if (< loop_a_count error_code_length)
         (loop
-         (add1 loop_count)
+         (add1 loop_a_count)
          (cons
-          (let xor-loop ([loop_list raw_list]
-                         [last_result loop_count])
-            (if (not (null? loop_list))
-                (xor-loop (cdr loop_list) (bitwise-xor last_result (car loop_list)))
-                last_result))
+          (let xor-loop ([loop_n_list raw_list]
+                         [last_result_n 0])
+            (if (not (null? loop_n_list))
+                (let ([ri (car loop_n_list)]
+                      [gf+ #f]
+                      [ntoa #f]
+                      [a+ #f]
+                      [aton #f])
+
+                  (set! gf+ (bitwise-xor last_result_n (car loop_n_list)))
+
+                  (set! ntoa (hash-ref (*gf_ntoa_map*) gf+ 0))
+
+                  (if (= ntoa 0)
+                      (begin
+                        (set! a+ 0)
+                        (set! aton 0))
+                      (begin
+                        (set! a+ (+ loop_a_count ntoa))
+
+                        (set! aton (hash-ref (*gf_aton_map*) (modulo a+ (*2^m_1*))))))
+
+                  (printf "last_n:[~a], ri:[~a], gf+:[~a], ntoa:[~a], a+:[~a], aton:[~a]\n" last_result_n ri gf+ ntoa a+ aton)
+
+                  (xor-loop (cdr loop_n_list) aton))
+                last_result_n))
           result_list))
         result_list)))
           
--- reed-solomon/tests/decode/syndrome-test.rkt
+++ reed-solomon/tests/decode/syndrome-test.rkt
@@ -2,21 +2,9 @@
 
 (require rackunit/text-ui)
 
+(require "../../lib/share/gf.rkt")
+
 (require rackunit "../../lib/decode/syndrome.rkt")
-
-(define (get-gf-aton-hash)
-  (let ([aton_map (make-hash)])
-    (let loop ([a 0]
-               [last_n (/ 1 2)])
-      (when (< a 15)
-            (let ([n (* last_n 2)])
-              (when (> n 15)
-                    (set! n (bitwise-xor n 19)))
-
-              (hash-set! aton_map a n)
-
-              (loop (add1 a) n))))
-    aton_map))
 
 (define test-syndrome
   (test-suite 
@@ -25,10 +13,17 @@
    (test-case
     "test-get-syndromes"
 
-    (check-equal? (get-syndromes 
-                   '(1 2 3 4 5 11 7 8 9 10 11 3 1 12 12)
-                   4)
-                  '(12 4 3 15)))
+    (parameterize*
+     ([*bit_width* 4]
+      [*2^m_1* (sub1 (expt 2 (*bit_width*)))]
+      [*primitive_poly_value* 19]
+      [*gf_aton_map* (get-gf-aton-hash)]
+      [*gf_ntoa_map* (make-hash (hash-map (*gf_aton_map*) (lambda (a n) (cons n a))))])
+
+     (check-equal? (get-syndromes 
+                    '(1 2 3 4 5 11 7 8 9 10 11 3 1 12 12)
+                    4)
+                   '(12 4 3 15))))
     ))
 
 (run-tests test-syndrome)
