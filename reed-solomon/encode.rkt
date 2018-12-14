3b037a17bf831a481e61cddac6b7e4662c968829
Chen Xiao <chenxiao770117@gmail.com>
2018-12-14 17:17:45 +0800

    update

--- reed-solomon/encode.rkt
+++ reed-solomon/encode.rkt
@@ -6,6 +6,7 @@
 (require "lib/encode/generator-poly.rkt")
 (require "lib/encode/long-division.rkt")
 (require "lib/encode/express/express.rkt")
+(require "lib/decode/euclidean.rkt")
 
 (provide (contract-out
           [encode (->* 
@@ -52,64 +53,66 @@
 
      (express express? (lambda () (write-report-long-division-start express_path)))
      
-     (let loop ([loop_message_n (prepare-message message_poly patrity_length)]
-                [count 1])
-       
-       (if (<= count message_length)
-           (let* ([step1_message_x #f]
-                  [step2_message_n #f]
-                  [step3_aligned_generator_by_x #f]
-                  [step4_message_n_to_a #f]
-                  [step5_multiply_a #f]
-                  [step6_to_n #f]
-                  [step7_xor_n #f]
-                  [step8_discard_first_zeros_n #f]
-                  [step9_zeros_count #f])
-
-             (set! step1_message_x (cdar (string-n->poly loop_message_n)))
-
-             (set! step2_message_n (caar (string-n->poly loop_message_n)))
-
-             (set! step3_aligned_generator_by_x (prepare-generator generator_poly step1_message_x))
-
-             (set! step4_message_n_to_a (hash-ref (*gf_ntoa_map*) step2_message_n))
-
-             (set! step5_multiply_a (poly-gf-a-multiply step3_aligned_generator_by_x (format "a~a" step4_message_n_to_a)))
-
-             (set! step6_to_n (poly-gf-a->n step5_multiply_a))
-
-             (set! step7_xor_n (poly-n-xor loop_message_n step6_to_n))
-
-             (set! step8_discard_first_zeros_n 
-                   (poly-n->string
-                    (let loop ([loop_list (string-n->poly step7_xor_n)])
-                      (if (= (caar loop_list) 0)
-                          (loop (cdr loop_list))
-                          loop_list))))
-             
-             (set! step9_zeros_count (- (length (string-n->poly loop_message_n)) (length (string-n->poly step8_discard_first_zeros_n))))
-
-             (express express? (lambda () 
-                                 (write-report-long-division-detail
-                                  count
-                                  generator_poly
-                                  loop_message_n
-                                  step1_message_x
-                                  step2_message_n
-                                  step3_aligned_generator_by_x
-                                  step4_message_n_to_a
-                                  step5_multiply_a
-                                  step6_to_n
-                                  step7_xor_n
-                                  step8_discard_first_zeros_n
-                                  step9_zeros_count
-                                  express_path)))
-             
-             (loop step8_discard_first_zeros_n (+ count step9_zeros_count)))
-           (let ([result
-                  (map
-                   (lambda (pair)
-                     (car pair))
-                   (string-n->poly loop_message_n))])
-             (express express? (lambda () (write-report-error-code bit_width result express_path)))
-             result))))))
+     (euc-divide (prepare-message message_poly patrity_length) (poly-gf-a->n generator_poly)))))
+     
+;     (let loop ([loop_message_n (prepare-message message_poly patrity_length)]
+;                [count 1])
+;       
+;       (if (<= count message_length)
+;           (let* ([step1_message_x #f]
+;                  [step2_message_n #f]
+;                  [step3_aligned_generator_by_x #f]
+;                  [step4_message_n_to_a #f]
+;                  [step5_multiply_a #f]
+;                  [step6_to_n #f]
+;                  [step7_xor_n #f]
+;                  [step8_discard_first_zeros_n #f]
+;                  [step9_zeros_count #f])
+;
+;             (set! step1_message_x (cdar (string-n->poly loop_message_n)))
+;
+;             (set! step2_message_n (caar (string-n->poly loop_message_n)))
+;
+;             (set! step3_aligned_generator_by_x (prepare-generator generator_poly step1_message_x))
+;
+;             (set! step4_message_n_to_a (hash-ref (*gf_ntoa_map*) step2_message_n))
+;
+;             (set! step5_multiply_a (poly-gf-a-multiply step3_aligned_generator_by_x (format "a~a" step4_message_n_to_a)))
+;
+;             (set! step6_to_n (poly-gf-a->n step5_multiply_a))
+;
+;             (set! step7_xor_n (poly-n-xor loop_message_n step6_to_n))
+;
+;             (set! step8_discard_first_zeros_n 
+;                   (poly-n->string
+;                    (let loop ([loop_list (string-n->poly step7_xor_n)])
+;                      (if (= (caar loop_list) 0)
+;                          (loop (cdr loop_list))
+;                          loop_list))))
+;             
+;             (set! step9_zeros_count (- (length (string-n->poly loop_message_n)) (length (string-n->poly step8_discard_first_zeros_n))))
+;
+;             (express express? (lambda () 
+;                                 (write-report-long-division-detail
+;                                  count
+;                                  generator_poly
+;                                  loop_message_n
+;                                  step1_message_x
+;                                  step2_message_n
+;                                  step3_aligned_generator_by_x
+;                                  step4_message_n_to_a
+;                                  step5_multiply_a
+;                                  step6_to_n
+;                                  step7_xor_n
+;                                  step8_discard_first_zeros_n
+;                                  step9_zeros_count
+;                                  express_path)))
+;             
+;             (loop step8_discard_first_zeros_n (+ count step9_zeros_count)))
+;           (let ([result
+;                  (map
+;                   (lambda (pair)
+;                     (car pair))
+;                   (string-n->poly loop_message_n))])
+;             (express express? (lambda () (write-report-error-code bit_width result express_path)))
+;             result))))))
