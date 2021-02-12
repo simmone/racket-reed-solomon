23477ad9da6a8994641526888564fa4ac9bd621f
Chen Xiao <chenxiao770117@gmail.com>
2021-02-12 08:07:26 +0800

    update

--- reed-solomon/info.rkt
+++ reed-solomon/info.rkt
@@ -3,6 +3,6 @@
   '(("scribble/reed-solomon.scrbl" (multi-page) (tool 100))))
 
 (define compile-omit-paths '("tests ref"))
-(define test-include-paths '("tests"))
+(define test-includes-paths '("tests"))
 
 
