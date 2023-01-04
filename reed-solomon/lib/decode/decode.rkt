546851d8f9e37d0361a54ec470ba5ff34c453749
Chen Xiao <chenxiao770117@gmail.com>
2023-01-04 10:46:39 +0800

    update

--- reed-solomon/lib/decode/decode.rkt
+++ reed-solomon/lib/decode/decode.rkt
@@ -11,12 +11,10 @@
 (require "forney.rkt")
 (require "correct-error.rkt")
 
-(require "express/express.rkt")
-
 (provide (contract-out
           [rs-decode (->* 
                       ((listof exact-integer?) natural?) 
-                      (#:bit_width natural? #:primitive_poly_value natural? #:express? boolean? #:express_path path-string?) 
+                      (#:bit_width natural? #:primitive_poly_value natural?) 
                       (listof exact-integer?))]
           ))
 
@@ -24,74 +22,49 @@
          raw_list
          patrity_length
          #:bit_width [bit_width 8]
-         #:primitive_poly_value [primitive_poly_value 285]
-         #:express? [express? #f]
-         #:express_path [express_path ".decode.express"])
+         #:primitive_poly_value [primitive_poly_value 285])
 
   (parameterize*
-   ([*express?* express?]
-    [*express_path* express_path]
-    [*bit_width* bit_width]
+   ([*bit_width* bit_width]
     [*2^m_1* (sub1 (expt 2 (*bit_width*)))]
     [*primitive_poly_value* primitive_poly_value]
     [*gf_aton_map* (get-gf-aton-hash)]
     [*gf_ntoa_map* (make-hash (hash-map (*gf_aton_map*) (lambda (a n) (cons n a))))])
 
-   (dynamic-wind
-       (lambda ()
-         (express-start))
-       (lambda ()
-         (express-input raw_list patrity_length bit_width primitive_poly_value)
+   (let (
+         [t #f]
+         [syndromes #f]
+         [syndrome_poly #f]
+         [lam_derivative_poly #f]
+         [Yj_poly #f]
+         [err_places #f]
+         [err_correct_pairs #f]
+         [corrected_values #f]
+         )
+     
+     (set! t (floor (/ patrity_length 2)))
 
-         (let (
-               [t #f]
-               [syndromes #f]
-               [syndrome_poly #f]
-               [lam_derivative_poly #f]
-               [Yj_poly #f]
-               [err_places #f]
-               [err_correct_pairs #f]
-               [corrected_values #f]
-               )
-           
-           (set! t (floor (/ patrity_length 2)))
+     (set! syndromes (get-syndromes raw_list (* 2 t) #t))
 
-           (set! syndromes (get-syndromes raw_list (* 2 t) #t))
+     (set! syndrome_poly (coeffients->poly-n syndromes))
+     
+     (if (= (foldr + 0 syndromes) 0)
+         raw_list
+         (with-handlers
+          ([exn:fail?
+            (lambda (v)
+              raw_list)])
+          (let-values ([(ome_poly lam_poly) (error-locator-poly syndrome_poly t #t)])
+            (set! err_places (chien-search lam_poly))
+            
+            (set! err_correct_pairs (forney lam_poly ome_poly err_places #t))
 
-           (set! syndrome_poly (coeffients->poly-n syndromes))
-           
-           (express-syndrome-poly syndrome_poly)
-           
-           (if (= (foldr + 0 syndromes) 0)
-               (begin
-                 (express-no-error)
-                 raw_list)
-               (with-handlers
-                ([exn:fail?
-                  (lambda (v)
-                    (express-too-many-errors v)
-                    raw_list)])
-                (let-values ([(ome_poly lam_poly) (error-locator-poly syndrome_poly t #t)])
-                  (set! err_places (chien-search lam_poly))
-                  
-                  (express-chien-search err_places)
+            (set! corrected_values 
+                  (correct-error 
+                   raw_list
+                   err_correct_pairs))
 
-                  (set! err_correct_pairs (forney lam_poly ome_poly err_places #t))
-
-                  (set! corrected_values 
-                        (correct-error 
-                         raw_list
-                         err_correct_pairs))
-
-                  (express-finally corrected_values bit_width)
-
-                  corrected_values)))))
-       (lambda ()
-         (express-galois-fields (*gf_aton_map*) (*gf_ntoa_map*))
-
-         (express-primitive-poly)
-
-         (express-euclidean-decode)))))
+            corrected_values))))))
 
 
 
--- reed-solomon/lib/decode/syndrome.rkt
+++ reed-solomon/lib/decode/syndrome.rkt
@@ -8,60 +8,38 @@
           ))
 
 (define (get-syndromes raw_list error_code_length [need-express? #f])
-  (let ([out (open-output-nowhere)])
-    (when (and need-express? (*express?*))
-      (let* ([scrbl_dir (build-path (*express_path*) "syndrome")]
-             [scrbl_file (build-path scrbl_dir "syndrome.scrbl")])
+  (let loop ([loop_a_count 0]
+             [result_list '()])
+    (if (< loop_a_count error_code_length)
+        (begin
+          (loop
+           (add1 loop_a_count)
+           (cons
+            (let xor-loop ([loop_n_list raw_list]
+                           [last_result_n 0])
+              (if (> (length loop_n_list) 1)
+                  (let ([ri (car loop_n_list)]
+                        [gf+ #f]
+                        [ntoa #f]
+                        [a+ #f]
+                        [aton #f])
 
-        (with-output-to-file
-            (build-path (*express_path*) "report.scrbl") #:exists 'append
-            (lambda ()
-              (printf "@include-section[\"syndrome/syndrome.scrbl\"]\n\n")))
+                    (set! gf+ (bitwise-xor last_result_n (car loop_n_list)))
 
-        (make-directory* scrbl_dir)
-        
-        (set! out (open-output-file scrbl_file #:exists 'replace))
+                    (set! ntoa (hash-ref (*gf_ntoa_map*) gf+ 0))
 
-        (fprintf out "#lang scribble/base\n\n")
+                    (if (= gf+ 0)
+                        (begin
+                          (set! a+ 0)
+                          (set! aton 0))
+                        (begin
+                          (set! a+ (+ loop_a_count ntoa))
 
-        (fprintf out "@title{Get Syndromes}\n\n")))
-    
-    (let loop ([loop_a_count 0]
-               [result_list '()])
-      (if (< loop_a_count error_code_length)
-          (begin
-            (fprintf out "@section{S~a}\n" loop_a_count)
-            (loop
-             (add1 loop_a_count)
-             (cons
-              (let xor-loop ([loop_n_list raw_list]
-                             [last_result_n 0])
-                (if (> (length loop_n_list) 1)
-                    (let ([ri (car loop_n_list)]
-                          [gf+ #f]
-                          [ntoa #f]
-                          [a+ #f]
-                          [aton #f])
+                          (set! aton (hash-ref (*gf_aton_map*) (modulo a+ (*2^m_1*))))))
+                    
+                    (xor-loop (cdr loop_n_list) aton))
+                  (let ([result (bitwise-xor last_result_n (car loop_n_list))])
+                    result)))
+            result_list)))
+        result_list)))
 
-                      (set! gf+ (bitwise-xor last_result_n (car loop_n_list)))
-
-                      (set! ntoa (hash-ref (*gf_ntoa_map*) gf+ 0))
-
-                      (if (= gf+ 0)
-                          (begin
-                            (set! a+ 0)
-                            (set! aton 0))
-                          (begin
-                            (set! a+ (+ loop_a_count ntoa))
-
-                            (set! aton (hash-ref (*gf_aton_map*) (modulo a+ (*2^m_1*))))))
-                      
-                      (fprintf out "@verbatim{( ~a + ~a ) x 2^~a = ~a}\n" last_result_n (car loop_n_list) loop_a_count aton)
-
-                      (xor-loop (cdr loop_n_list) aton))
-                    (let ([result (bitwise-xor last_result_n (car loop_n_list))])
-                      (fprintf out "@verbatim{ ~a + ~a = ~a}\n" last_result_n (car loop_n_list) result)
-                      result)))
-              result_list)))
-            result_list))))
-  
