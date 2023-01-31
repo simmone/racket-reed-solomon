e8cae4ab775e27b1cbb3c917d204892ca363b550
Chen Xiao <chenxiao770117@gmail.com>
2023-02-01 00:15:32 +0800

    update

--- reed-solomon/tests/field-math/euclidean-divide-explanation.rkt
+++ reed-solomon/tests/field-math/euclidean-divide-explanation.rkt
@@ -1,31 +1,35 @@
 #lang racket
 
-(require "../../field-math.rkt")
+(require "../../src/field-math.rkt")
 
 (define (euc-divide dividend divisor)
-  (let ([divisor_degree (cdr (poly-n-car divisor))])
-    (let loop ([loop_dividend dividend]
+  (let ([divisor_index (caar (poly->index_coe_pairs divisor))])
+    (printf "divisor_index:~a\n\n" divisor_index)
+
+    (let loop ([remainder dividend]
                [quotient_list '()])
       
-      (let ([loop_dividend_degree (cdr (poly-n-car loop_dividend))])
-        (if (and (not (string=? loop_dividend "")) (>= loop_dividend_degree divisor_degree))
+      (let ([remainder_degree (caar (poly->index_coe_pairs remainder))])
+        (if (>= remainder_degree divisor_degree)
             (let (
                   [loop_align_factor #f]
                   [loop_divisor_multiply_factor #f]
                   [loop_substract #f]
                   )
-              (set! loop_align_factor (poly-gf-n-divide-align divisor loop_dividend))
+              (set! loop_align_factor (poly-gf-n-divide-align divisor remainder))
               
               (set! loop_divisor_multiply_factor (poly-gf-n-multiply divisor loop_align_factor))
 
-              (set! loop_substract (poly-n-add loop_dividend loop_divisor_multiply_factor))
+              (set! loop_substract (poly-n-add remainder loop_divisor_multiply_factor))
               
               (loop loop_substract (cons loop_align_factor quotient_list)))
             (let ([quotient
                    (foldr (lambda (a b) (if b (string-append a "+" b) a)) #f (reverse quotient_list))])
               (values
                quotient
-               loop_dividend)))))))
+               remainder)))))))
+
+    ))
 
 (euc-divide "x4" "12x3+4x2+3x+15")
 
